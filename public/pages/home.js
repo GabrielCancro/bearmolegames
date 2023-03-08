d20a1b252a4024d46fd3db2f97925d9d2d0ba30f
GabrielCancro3OGS <119443886+GabrielCancro3OGS@users.noreply.github.com>
2023-03-08 14:07:25 -0300

    update

--- public/pages/home.js
+++ public/pages/home.js
@@ -4,7 +4,8 @@ import * as main from "../js/main.js";
 
 export async function initPage(){ 
 	var info = await get_user_info();
-    $("#tx_username").html(info.gamename+" - "+info.email);
+	$(".tx_username").html(info.gamename);
+    $(".tx_mail").html(info.email);	
     $("#btn_logout").click(async (e)=>{
         $("#btn_logout").addClass("inProgress");
         await fireutils.logout();  
